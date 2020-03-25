25be0fb202138527268e36fbdc5eb194c95c836a
Andrey Shipkov <shipkov2007@ya.ru>
2020-03-25 15:48:29 +0300

    Домашнее задание 5 исправленное

--- lesson5/lesson5_2.py
+++ lesson5/lesson5_2.py
@@ -1,40 +1,74 @@
 """
-2. Написать программу сложения и умножения двух шестнадцатеричных чисел. При этом каждое число представляется как массив, элементы которого это цифры числа.
-Например, пользователь ввёл A2 и C4F.
-Сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
-Сумма чисел из примера: [‘C’, ‘F’, ‘1’].
-Произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
+2. Написать программу сложения и умножения двух шестнадцатеричных чисел.
+При этом каждое число представляется как массив, элементы которого — цифры числа.
+Например, пользователь ввёл A2 и C4F. Нужно сохранить их как [‘A’, ‘2’] и [‘C’, ‘4’, ‘F’] соответственно.
+Сумма чисел из примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
+Примечание: Если воспользоваться функциями hex() и/или int() для преобразования систем счисления,
+задача решается в несколько строк. Для прокачки алгоритмического мышления такой вариант не подходит.
+Поэтому использование встроенных функций для перевода из одной системы счисления в другую в данной задаче под запретом.
+Вспомните начальную школу и попробуйте написать сложение и умножение в столбик.
 """
+from collections import deque
 
+digit_numbers = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+                 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15,
+                 0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+                 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
 
-def my_int(lst):
-    dic = {'0': 0,'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15 }
-    s = 0
-    for i, k in enumerate(reversed(lst)):
-        s += dic[k] * 16 ** i
-    return s
+def summ_sixteen(x, y):
 
+    result = deque()
+    transfer = 0
 
-def my_hex(n, s=''):
-    dic = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
-
-    s += str(dic[n % 16])
-
-    # print(str(int(num % 16)))
-
-    if n < 16:
-        return list(reversed(list(s)))
+    if len(y) > len(x):
+        x, y = deque(y), deque(x)
     else:
-        return my_hex(int(n // 16), s)
+        x, y = deque(x), deque(y)
+    while x:
+        if y:
+            output = digit_numbers[x.pop()] + digit_numbers[y.pop()] + transfer
+        else:
+            output = digit_numbers[x.pop()] + transfer
+        transfer = 0
+        if output < 16:
+            result.appendleft(digit_numbers[output])
+        else:
+            result.appendleft(digit_numbers[output - 16])
+            transfer = 1
+    if transfer:
+        result.appendleft('1')
+    return list(result)
 
+def multiplication_sixteen(x, y):
 
-#first_number = list(input('Первое шестнадцатеричное число: '))
-#second_number = list(input('Второе шестнадцатеричное число: '))
+    result = deque()
+    lot = deque([deque() for _ in range(len(y))])
+    x, y = x.copy(), deque(y)
 
-first_number_dec = my_int(list('A2'))
-second_number_dec = my_int(list('C4F'))
+    for i in range(len(y)):
+        a = digit_numbers[y.pop()]
+        for j in range(len(x) - 1, -1, -1):
+            lot[i].appendleft(a * digit_numbers[x[j]])
+        for _ in range(i):
+            lot[i].append(0)
 
-summa = my_hex(first_number_dec + second_number_dec)
-eqyal = my_hex(first_number_dec * second_number_dec)
+    transfer = 0
+    for _ in range(len(lot[-1])):
+        output = 0
+        for i in range(len(lot)):
+            if lot[i]:
+                output += lot[i].pop()
+        if output < 16:
+            result.appendleft(digit_numbers[output])
+        else:
+            result.appendleft(digit_numbers[output % 16])
+            transfer = output // 16
+    if transfer:
+        result.appendleft(digit_numbers[transfer])
+    return list(result)
 
-print(f'Сумма чисел {summa}, произведение {eqyal}')
+number1 = list(input('Введите число 1: ').upper())
+number2 = list(input('Введите число 2: ').upper())
+
+print(*number1, '+', *number2, '=', *summ_sixteen(number1, number2))
+print(*number1, '*', *number2, '=', *multiplication_sixteen(number1, number2))
